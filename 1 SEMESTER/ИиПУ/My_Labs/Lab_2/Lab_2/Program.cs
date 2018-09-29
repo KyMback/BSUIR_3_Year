d078c9d0d65044a462736e97db3bd5e47ba01f2c
KyMback <dark_AHrel@mail.ru>
2018-09-29 15:52:55 +0300

    Added lab3

--- 1 SEMESTER/ИиПУ/My_Labs/Lab_2/Lab_2/Program.cs
+++ 1 SEMESTER/ИиПУ/My_Labs/Lab_2/Lab_2/Program.cs
@@ -10,12 +10,12 @@ namespace Lab_2
     {
         static void Main(string[] args)
         {
-            var searcher13 = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
-
-            var list = searcher13.Get().Cast<ManagementObject>().ToArray();
+            var searcher = new ManagementObjectSearcher("root\\CIMV2", "SELECT * FROM Win32_DiskDrive");
+            var list = searcher.Get().Cast<ManagementObject>().ToArray();
 
             Tuple<long, long> allDrives = DriveInfo.GetDrives()
-                .Select(v => new Tuple<long, long>(v.AvailableFreeSpace, v.TotalSize)).Aggregate((l, r) =>
+                .Select(v => new Tuple<long, long>(v.AvailableFreeSpace, v.TotalSize))
+                .Aggregate((l, r) =>
                     new Tuple<long, long>(l.Item1 + r.Item1, l.Item2 + r.Item2));
 
 
@@ -27,11 +27,11 @@ namespace Lab_2
                 Console.WriteLine($"SerialNumber: {entity["SerialNumber"]}");
                 Console.WriteLine($"FirmwareRevision: {entity["FirmwareRevision"]}");
                 Console.WriteLine($"FreeSpace: {allDrives.Item1:N}");
-                Console.WriteLine($"BusySpace: {(allDrives.Item2 - allDrives.Item1):N}");
-                Console.WriteLine($"Size: {entity["Size"]:N}");
+                Console.WriteLine($"BusySpace: {(allDrives.Item2 - allDrives.Item1):N} bytes");
+                Console.WriteLine($"Size: {entity["Size"]:N} bytes");
                 Console.WriteLine($"InterfaceType: {entity["InterfaceType"]}");
-                Console.WriteLine(
-                    $"CapabilityDescriptions: {(entity["CapabilityDescriptions"] as IEnumerable<string>).Aggregate((l, r) => $"{l.ToString()}; {r.ToString()}")}");
+                //Console.WriteLine(
+                //    $"CapabilityDescriptions: {(entity["CapabilityDescriptions"] as IEnumerable<string>).Aggregate((l, r) => $"{l.ToString()}; {r.ToString()}")}");
                 Console.WriteLine("------------------------------------------------------------------");
             }
         }
---
+++ 1 SEMESTER/ИиПУ/My_Labs/Lab_3/Lab_3/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.ComponentModel;
+using System.Linq;
+using System.Management;
+using System.Threading;
+using System.Windows.Forms;
+using Microsoft.Win32;
+
+namespace Lab_3
+{
+    public class Program
+    {
+        public PowerLineStatus PowerLineStatus { get; private set; }
+
+        public float BatteryLifePercent { get; private set; }
+        
+        public static void Main(string[] args)
+        {
+            var program = new Program();
+            while (true)
+            {
+                program.WriteStatus(SystemInformation.PowerStatus.PowerLineStatus,
+                    SystemInformation.PowerStatus.BatteryLifePercent);
+            }
+        }
+
+        private void WriteStatus(PowerLineStatus status, float batteryLifePercent)
+        {
+            if (PowerLineStatus == status && BatteryLifePercent == batteryLifePercent)
+            {
+                Thread.Sleep(1000);
+                return;
+            }
+            
+            Console.Clear();
+            Console.WriteLine($"Power supply type: {GetPowerSupplyType(status)}");
+            Console.WriteLine($"Battery level: {batteryLifePercent:P}");
+            
+            BatteryLifePercent = batteryLifePercent;
+            PowerLineStatus = status;
+        }
+
+        private static string GetPowerSupplyType(PowerLineStatus status)
+        {
+            switch (status)
+            {
+                    case PowerLineStatus.Offline: return "Battery";
+                    case PowerLineStatus.Online: return "External power";
+                    default: throw new InvalidEnumArgumentException("Not supported power supply type");
+            }
+        }
+    }
+}
