57e9706487dc53d321b0b5b0da89edf6fbce1115
KyMback <dark_AHrel@mail.ru>
2018-10-12 12:30:29 +0300

    Added lab 4

---
+++ 1 SEMESTER/ИиПУ/My_Labs/Lab_4/Lab_4/Lab_4/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Linq;
+using System.Management;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+
+namespace Lab_4
+{
+    public class Program
+    {
+        private const int SW_HIDE = 0;
+
+        [DllImport("kernel32.dll")]
+        private static extern IntPtr GetConsoleWindow();
+
+        [DllImport("user32.dll")]
+        private static extern bool ShowWindow(IntPtr hWnd, int nCmdShow);
+
+        private static void Main()
+        {
+            ManagementObjectSearcher search = new ManagementObjectSearcher("SELECT * From Win32_PnPEntity");
+            var webcams = search.Get().Cast<ManagementBaseObject>().Select(c => new
+            {
+                Caption = Convert.ToString(c["Caption"]),
+                Manufacturer = Convert.ToString(c["Manufacturer"]),
+                DeviceId = Convert.ToString(c["DeviceId"]),
+                Name = Convert.ToString(c["Name"])
+            }).Where(w => w.Caption.Contains("Camera")).ToArray();
+            var webcam = webcams
+                .First(w => w.Caption.Contains("RGB"));
+            
+            Console.WriteLine($"Caption : {webcam.Caption}");
+            Console.WriteLine($"Device Id : {webcam.DeviceId}");
+            Console.WriteLine($"Name : {webcam.Name}");
+            Console.WriteLine($"Manufacturer : {webcam.Manufacturer}");
+
+            var capture = new VideoCapture(1);
+            Console.WriteLine($"Width of camera : {Convert.ToInt32(capture.GetCaptureProperty(CapProp.FrameWidth))}");
+            Console.WriteLine($"Height of camera : {Convert.ToInt32(capture.GetCaptureProperty(CapProp.FrameHeight))}");
+            capture.Dispose();
+
+            HideApp();
+            Application.Run(new VideoSpyAdapter());
+        }
+
+        private static void HideApp()
+        {
+            Console.WriteLine("Press any key to hide console window");
+            Console.ReadKey();
+            IntPtr handle = GetConsoleWindow();
+            ShowWindow(handle, SW_HIDE);
+        }
+    }
+}
---
+++ 1 SEMESTER/ИиПУ/My_Labs/Lab_4/Lab_4/Lab_4/VideoSpyAdapter.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Drawing;
+using System.Windows.Forms;
+using Emgu.CV;
+using Emgu.CV.CvEnum;
+using Gma.System.MouseKeyHook;
+
+namespace Lab_4
+{
+    public class VideoSpyAdapter : Form
+    {
+        private readonly int defaultNumberOfFrames = 25;
+        private readonly string fileName = "spyCameraObject";
+        private readonly int numberOfSeconds = 5;
+        private readonly string extensionForImage = ".jpg";
+        private readonly string extensionForVideo = ".mp4";
+        private readonly int defaultCameraNumber = 1;
+
+        public VideoSpyAdapter()
+        {
+            HideWindow();
+            IKeyboardMouseEvents keyboardEvents = Hook.GlobalEvents();
+            keyboardEvents.KeyDown += KeyDownEvent;
+        }
+
+        private void HideWindow()
+        {
+            base.SetVisibleCore(false);
+            Visible = false;
+            ShowInTaskbar = false;
+            WindowState = FormWindowState.Minimized;
+            Hide();
+        }
+
+        private void KeyDownEvent(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyData)
+            {
+                case Keys.F7:
+                {
+                    var capture = new VideoCapture(defaultCameraNumber);
+                    capture.QueryFrame().Bitmap.Save(fileName + extensionForImage);
+                    capture.Dispose();
+                    break;
+                }
+                case Keys.F8:
+                {
+                    var capture = new VideoCapture(defaultCameraNumber);
+
+                    int width = Convert.ToInt32(capture.GetCaptureProperty(CapProp.FrameWidth));
+                    int height = Convert.ToInt32(capture.GetCaptureProperty(CapProp.FrameHeight));
+                    var videoWriter = new VideoWriter(fileName + extensionForVideo, defaultNumberOfFrames,
+                        new Size(width, height), true);
+
+                    var mat = new Mat();
+                    int frameNumber = 0;
+                    while (frameNumber < defaultNumberOfFrames * numberOfSeconds)
+                    {
+                        capture.Read(mat);
+                        videoWriter.Write(mat);
+                        frameNumber++;
+                    }
+
+                    if (videoWriter.IsOpened)
+                    {
+                        videoWriter.Dispose();                        
+                    }
+                    capture.Dispose();
+                    break;
+                }
+            }
+        }
+    }
+}
