add58715a0cae6aba7bca2b7aa1526bc801eb419
KyMback <dark_AHrel@mail.ru>
2018-11-14 21:52:33 +0300

    added toks 4

---
+++ 1 SEMESTER/ТОКС/My_Labs/Lab_4/Lab_4/Lab_4/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿namespace Lab_4
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.InputTextBox = new System.Windows.Forms.TextBox();
+            this.contextMenuStrip1 = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.Input = new System.Windows.Forms.Label();
+            this.OutputTextBox = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.DebugTextBox = new System.Windows.Forms.TextBox();
+            this.SendButton = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // InputTextBox
+            // 
+            this.InputTextBox.Location = new System.Drawing.Point(12, 39);
+            this.InputTextBox.Name = "InputTextBox";
+            this.InputTextBox.Size = new System.Drawing.Size(272, 20);
+            this.InputTextBox.TabIndex = 0;
+            // 
+            // contextMenuStrip1
+            // 
+            this.contextMenuStrip1.Name = "contextMenuStrip1";
+            this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            // 
+            // Input
+            // 
+            this.Input.AutoSize = true;
+            this.Input.Location = new System.Drawing.Point(12, 23);
+            this.Input.Name = "Input";
+            this.Input.Size = new System.Drawing.Size(31, 13);
+            this.Input.TabIndex = 2;
+            this.Input.Text = "Input";
+            this.Input.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // OutputTextBox
+            // 
+            this.OutputTextBox.Location = new System.Drawing.Point(12, 95);
+            this.OutputTextBox.Multiline = true;
+            this.OutputTextBox.Name = "OutputTextBox";
+            this.OutputTextBox.ReadOnly = true;
+            this.OutputTextBox.Size = new System.Drawing.Size(272, 122);
+            this.OutputTextBox.TabIndex = 3;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(12, 79);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(39, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Output";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(12, 231);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(39, 13);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "Debug";
+            this.label2.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            // 
+            // DebugTextBox
+            // 
+            this.DebugTextBox.Location = new System.Drawing.Point(12, 247);
+            this.DebugTextBox.Multiline = true;
+            this.DebugTextBox.Name = "DebugTextBox";
+            this.DebugTextBox.ReadOnly = true;
+            this.DebugTextBox.Size = new System.Drawing.Size(272, 122);
+            this.DebugTextBox.TabIndex = 5;
+            // 
+            // SendButton
+            // 
+            this.SendButton.Location = new System.Drawing.Point(209, 65);
+            this.SendButton.Name = "SendButton";
+            this.SendButton.Size = new System.Drawing.Size(75, 23);
+            this.SendButton.TabIndex = 7;
+            this.SendButton.Text = "Send";
+            this.SendButton.UseVisualStyleBackColor = true;
+            this.SendButton.Click += new System.EventHandler(this.SendButton_Click);
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(209, 375);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Clear";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(299, 398);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.SendButton);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.DebugTextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.OutputTextBox);
+            this.Controls.Add(this.Input);
+            this.Controls.Add(this.InputTextBox);
+            this.MaximumSize = new System.Drawing.Size(315, 437);
+            this.MinimumSize = new System.Drawing.Size(315, 437);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox InputTextBox;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip1;
+        private System.Windows.Forms.Label Input;
+        private System.Windows.Forms.TextBox OutputTextBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox DebugTextBox;
+        private System.Windows.Forms.Button SendButton;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ 1 SEMESTER/ТОКС/My_Labs/Lab_4/Lab_4/Lab_4/Form1.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace Lab_4
+{
+    public partial class Form1 : Form
+    {
+        public Random Random { get; set; }
+
+        private const int Pwd = 50;
+        private const int MaximumAttempts = 10;
+        private const string CollisionSymbol = "X";
+
+        private bool IsChannelFree() => Random.Next(0, 2) == 1;
+
+        private bool IsCollisionOccured() => Random.Next(0, 3) != 1;
+
+        private int GetDelayForCollisionWaiting(int count) =>
+            Random.Next(0, (int) Math.Pow(2, Math.Min(count, MaximumAttempts))) * 10;
+
+        public Form1()
+        {
+            InitializeComponent();
+            Random = new Random();
+        }
+
+        private void SendButton_Click(object sender, EventArgs e)
+        {
+            if (string.IsNullOrEmpty(InputTextBox.Text))
+            {
+                return;
+            }
+            var data = InputTextBox.Text.ToCharArray();
+            InputTextBox.Clear();
+
+            foreach (var symbol in data)
+            {
+                int counter = 0;
+                while (!TryToSend(symbol))
+                {
+                    counter++;
+                    if (counter > MaximumAttempts)
+                    {
+                        break;
+                    }
+                    Thread.Sleep(GetDelayForCollisionWaiting(counter));
+                }
+                DebugTextBox.AppendText(Environment.NewLine);
+            }
+        }
+
+        private bool TryToSend(char data)
+        {
+            while (!IsChannelFree()) { }
+            Thread.Sleep(Pwd);
+            if (IsCollisionOccured())
+            {
+                DebugTextBox.AppendText(CollisionSymbol);
+                return false;
+            }
+            OutputTextBox.AppendText(data + Environment.NewLine);
+            return true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            InputTextBox.Clear();
+            OutputTextBox.Clear();
+            DebugTextBox.Clear();
+        }
+    }
+}
